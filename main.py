f22fd251707c6176a89573482bb64198f8e94a43
Terrain Duck <mathilda1337@tutanota.com>
2022-06-19 19:25:26 +0200

    Implement purchase

--- main.py
+++ main.py
@@ -1,28 +1,69 @@
-# TODO: Add error handling
-# Write output to log if success or not
+#!/usr/bin/env python
+
+"""
+TradeOgre API Dollar Cost Average Script
+https://github.com/terrainduck/tradeogre-dca
+"""
 
 import requests
+from requests.auth import HTTPBasicAuth
 import json
+import datetime
 
-API_URL = "https://tradeogre.com/api/v1/"
 
 # Get config
 configFile = open("config.json", "r")
-config = json.loads(configFile.read())[0] # TODO: Loop through all the items
+configRead = configFile.read()
+configJson = json.loads(configRead)
+config = configJson[0]
 configFile.close()
 
-market = config["market"]
-amountDollar = config["amountDollar"]
-maxPrice = config["maxPrice"]
+publicKey = config["keys"]["public"]
+secretKey = config["keys"]["secret"]
 
-orders = requests.get(API_URL + "orders/" + market).json()
+markets = config["markets"]
 
-sellOrders = orders["sell"]
+now = datetime.datetime.now()
+print(str(now))
 
-minSellOrderPrice = min(sellOrders.keys())
+API_URL = config["api_url"]
 
-if float(minSellOrderPrice) < float(maxPrice):
-    print("buy")
-else:
-    print("don't buy")
+for market in markets:
+    pair = market["pair"]
+    quantity = market["quantity"]
+    maxPrice = market["maxPrice"]
 
+    try:
+        orders = requests.get(API_URL + "orders/" + pair).json()
+    except Exception as e:
+        print(e)
+        exit()
+
+    if (not orders["success"]):
+        print(orders)
+        exit()
+
+    sellOrders = orders["sell"]
+
+    minSellOrderPrice = min(sellOrders.keys())
+
+    if float(minSellOrderPrice) < float(maxPrice):
+        data = {
+            "market": pair,
+            "quantity": quantity,
+            "price": minSellOrderPrice
+        }
+
+        URL = API_URL + "order/buy"
+
+        try:
+            response = requests.post(url=URL, data=data, auth=HTTPBasicAuth(publicKey, secretKey))
+        except Exception as e:
+            print(e)
+            exit()
+
+        print(response.text)
+    else:
+        print("Minimum sell order price is not less than max buy price. Not buying.")
+
+print("")
